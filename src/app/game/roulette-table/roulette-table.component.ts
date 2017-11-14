0cb544db54c883b979348a8b19d87e636831902b
Kartik Sharma <kartik.sharma1607@outlook.com>
2017-11-14 16:13:46 +0530

    Added bindings to table

--- src/app/game/roulette-table/roulette-table.component.ts
+++ src/app/game/roulette-table/roulette-table.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouletteTableComponent implements OnInit {
 
-  constructor() { }
+  chipCount: number[];
+  constants = {
+    iZERO: 0,
+    iFIRST_TWELVE: 1,
+    iSECOND_TWELVE: 2,
+    iTHIRD_TWELVE: 3,
+    iFIRST_EIGHTEEN: 4,
+    iLAST_EIGHTEEN: 5,
+    iODD: 6,
+    iEVEN: 7,
+  }
+
+  constructor() {
+    this.chipCount  = [0, 0, 0, 0, 0, 0, 0, 0];
+  }
 
   ngOnInit() {
+  }
+
+  minusOne(index) {
+    if (this.chipCount[index] === 0) {
+      return;
+    }
+    this.chipCount[index]--;
+  }
+
+  plusOne(index) {
+    this.chipCount[index]++;
   }
 
 }
